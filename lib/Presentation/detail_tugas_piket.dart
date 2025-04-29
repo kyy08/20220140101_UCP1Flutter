90580280175be1d5398d1f94f6956892b9a7a341
kyy08 <kyyelzz1@gmail.com>
2025-04-29 18:39:58 +0700

    membuat DetailTugasPiketPage

---
+++ lib/Presentation/detail_tugas_piket.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class DetailTugasPiketPage extends StatelessWidget {
+  final Map<String, String> tugas;
+
+  const DetailTugasPiketPage({super.key, required this.tugas});
+
+  
