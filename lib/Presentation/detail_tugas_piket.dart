2a36ece27e053442225bf00d731d610bd5a7245b
kyy08 <kyyelzz1@gmail.com>
2025-04-29 18:40:37 +0700

    membuat form di DetailTugasPiket

--- lib/Presentation/detail_tugas_piket.dart
+++ lib/Presentation/detail_tugas_piket.dart
@@ -5,4 +5,30 @@ class DetailTugasPiketPage extends StatelessWidget {
 
   const DetailTugasPiketPage({super.key, required this.tugas});
 
-  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Detail Tugas'),
+        backgroundColor: Colors.orange,
+        foregroundColor: Colors.white,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Nama Anggota:', style: TextStyle(fontWeight: FontWeight.bold)),
+            Text(tugas['nama'] ?? '-'),
+            SizedBox(height: 16),
+            Text('Tanggal:', style: TextStyle(fontWeight: FontWeight.bold)),
+            Text(tugas['tanggal'] ?? '-'),
+            SizedBox(height: 16),
+            Text('Tugas:', style: TextStyle(fontWeight: FontWeight.bold)),
+            Text(tugas['tugas'] ?? '-'),
+          ],
+        ),
+      ),
+    );
+  }
+}
